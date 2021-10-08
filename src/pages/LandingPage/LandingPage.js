66aa156bd237bc7784f1b6b5bed63fc941a3ca11
daniekeys <starlordflash@gmail.com>
2021-10-08 03:55:16 -0700

    updater

--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -1,10 +1,18 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
+import {Button} from '@material-ui/core'
 
 const LandingPage = () => {
     return (
-        <div>
-           i am the landing page 
-        </div>
+        <>
+           <Button component={Link} to='register' variant='outlined' color='primary'>
+            Register
+               </Button>
+           <Button component={Link} to='login' variant='outlined' color='secondary'>
+            Login
+               </Button>
+
+        </>
     )
 }
 
